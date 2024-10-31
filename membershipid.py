9c3bd75566ab994a947b6713db04607e95ee4204
TONY THOMAS <134082850+TONY7990@users.noreply.github.com>
2024-10-31 14:37:18 +0530

    Create membershipid.py

---
+++ membershipid.py
@@ -0,0 +1,22 @@
+# initialized some sequences
+list1 = [1, 2, 3, 4, 5]
+str1 = "Hello World"
+set1 = {1, 2, 3, 4, 5}
+dict1 = {1: "Geeks", 2:"for", 3:"geeks"}
+tup1 = (1, 2, 3, 4, 5)
+
+# using membership 'in' operator
+# checking an integer in a list
+print(2 in list1)
+
+# checking a character in a string
+print('O' in str1)
+
+# checking an integer in aset
+print(6 in set1)
+
+# checking for a key in a dictionary
+print(3 in dict1)
+
+# checking for an integer in a tuple
+print(9 in tup1)
